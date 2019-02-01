15c303caec7245e37e2ff8efa9ee6113b87c4961
Luteces <ebompuis.dev@gmail.com>
2019-02-01 14:45:07 +0100

    Bootstrap
    Added boostrap 4 into the project
    Code was indented with prettier

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React, { Component} from  "react";
-import Login from "./Component/Login" //Always capital letter
-import "./App.css" //My CSS
+import React, { Component } from "react";
+import Login from "./Component/Login"; //Always capital letter
+import "./App.css"; //My CSS
 class App extends Component {
-    render() {
-        return(
-            <div>
-            <h1>Hi, You have access to your Avatar</h1>
-            <p>Please enter your pseudo into the field</p>
-                <Login/>
-            </div>
-
-        )
-    }
+  render() {
+    return (
+      <div class="jumbotron-fluid">
+        <div class="container">
+          <h1>Hi, You have access to your Avatar</h1>
+          <p>Please enter your pseudo into the field</p>
+          <Login />
+        </div>
+      </div>
+    );
+  }
 }
 export default App;
--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,30 +1,34 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 
-class Login extends Component{
-    constructor(props){
-        super(props);
-        let avatar ="";
-        this.state={avatar};
-    }
-    getAvatar =()=>{
-        const pseudo = document.getElementById("pseudo").value;
-        // console.log("https://api.github.com/search/users?q=" + pseudo);
-        fetch("https://api.github.com/search/users?q=" + pseudo)
-            .then((success)=> success.json())
-            .then((success)=>{
-               // console.log(success.items[0].avatar_url);
-               this.setState({avatar: success.items[0].avatar_url});
-            });
-    };
-    render() {
-        return(
-            <div>
-               <input id="pseudo" type="text"/>
-               <input type="button" value="Click Here" onClick={this.getAvatar}/>
-               <img src={this.state.avatar} alt={""}/>
-            </div>
-
-        )
-    }
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    let avatar = "";
+    this.state = { avatar };
+  }
+  getAvatar = () => {
+    const pseudo = document.getElementById("pseudo").value;
+    // console.log("https://api.github.com/search/users?q=" + pseudo);
+    fetch("https://api.github.com/search/users?q=" + pseudo)
+      .then(success => success.json())
+      .then(success => {
+        // console.log(success.items[0].avatar_url);
+        this.setState({ avatar: success.items[0].avatar_url });
+      });
+  };
+  render() {
+    return (
+      <div class="container">
+        <input id="pseudo" type="text" />
+        <input
+          type="button"
+          class="btn btn-success"
+          value="Click Here"
+          onClick={this.getAvatar}
+        />
+        <img src={this.state.avatar} alt={""} />
+      </div>
+    );
+  }
 }
 export default Login;
